e8998cc6d5a94b3132d91d601ee63ee632880fde
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-03-30 20:36:23 +0200

    implenmenteringen av crypterings algoritmen för aes - String

--- src/modules/crypterings/Cryptaes.java
+++ src/modules/crypterings/Cryptaes.java
@@ -1,17 +1,39 @@
 package modules.crypterings;
 
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 public class Cryptaes {
 
 
     public static String Stringcry(IvParameterSpec iv, SecretKey key, String masige) {
-
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(1, key, iv);
+            byte[] cipherText = cipher.doFinal(masige.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(cipherText);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
         return null;
     }
 
     public static String Stringdicry(IvParameterSpec iv, SecretKey key, String masige) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(2, key, iv);
+            byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(masige));
+            return new String(plainText);
+        } catch (InvalidAlgorithmParameterException | NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException | InvalidKeyException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
         return null;
     }
 }
