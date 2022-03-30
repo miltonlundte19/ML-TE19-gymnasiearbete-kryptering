245478e8b47e53ea5fc6b5fc9a1514986c6fabdc
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-03-30 20:18:28 +0200

    början på modelen för crypteringen

---
+++ src/modules/Crypteringsmodule.java
@@ -0,0 +1,49 @@
+package modules;
+
+import modules.crypterings.Cryptaes;
+import setings.Settings;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Crypteringsmodule {
+    private Settings settings;
+    private FileWriter looger;
+    private byte[] id;
+    private String masige;
+    private IvParameterSpec iv;
+    private SecretKey key;
+    public Crypteringsmodule(Settings settings) {
+        this.id = settings.getId();
+        this.masige = settings.getPlainText();
+        this.iv = settings.getIv();
+        this.key = settings.getKey();
+        this.settings = settings;
+    }
+
+    public void setLooger(FileWriter looger) {
+        this.looger = looger;
+    }
+
+    public void start() {
+        if (id[0] == 1) {
+            if (masige != null) {
+                try {
+                    looger.write("string:" + settings.getPlainText() + "\ntiden crypteringen började: \n" + System.nanoTime());
+                    String encryptmesig;
+                    if (id[1] == 1) {
+                        encryptmesig = Cryptaes.Stringcry(iv, key, masige);
+                    } else {
+                        encryptmesig = Cryptaes.Stringdicry(iv,key, masige);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(-1);
+                }
+            }
+        }
+    }
+}
--- src/modules/crypteringsmodule.java
+++
@@ -1,4 +0,0 @@
-package modules;
-
-public class crypteringsmodule {
-}
