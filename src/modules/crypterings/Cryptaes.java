ca410c71475049e7ec6352e9b22a5a85577da47b
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-03-30 20:17:47 +0200

    början på aes crypterings klasserna/kod

---
+++ src/modules/crypterings/Cryptaes.java
@@ -0,0 +1,17 @@
+package modules.crypterings;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+public class Cryptaes {
+
+
+    public static String Stringcry(IvParameterSpec iv, SecretKey key, String masige) {
+
+        return null;
+    }
+
+    public static String Stringdicry(IvParameterSpec iv, SecretKey key, String masige) {
+        return null;
+    }
+}
