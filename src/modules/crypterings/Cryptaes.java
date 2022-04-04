eee3fd6b1bb7e173241f216bebcc3f23e5474aa9
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-04-04 20:56:54 +0200

    crypterings modelen för att cryptera en fill

--- src/modules/crypterings/Cryptaes.java
+++ src/modules/crypterings/Cryptaes.java
@@ -2,6 +2,7 @@ package modules.crypterings;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -35,5 +36,32 @@ public class Cryptaes {
             System.exit(-1);
         }
         return null;
+    }
+
+    public static void Filebufercry(byte mode, IvParameterSpec iv, SecretKey key, File in, File ou) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(mode, key, iv);
+            FileInputStream inputStream = new FileInputStream(in);
+            FileOutputStream outputStream = new FileOutputStream(ou);
+            byte[] buffer = new byte[64];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byte[] output = cipher.update(buffer, 0, bytesRead);
+                if (output != null) {
+                    outputStream.write(output);
+                }
+            }
+            byte[] outputBytes = cipher.doFinal();
+            if (outputBytes != null) {
+                outputStream.write(outputBytes);
+            }
+            inputStream.close();
+            outputStream.flush();
+            outputStream.close();
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException | IllegalBlockSizeException | IOException | BadPaddingException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
