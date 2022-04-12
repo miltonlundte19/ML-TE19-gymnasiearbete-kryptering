08cfe8af2427206a6b2c506ce42ceb2247217b91
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-04-12 17:26:17 +0200

    organisera

--- src/modules/Crypteringsmodule.java
+++ src/modules/Crypteringsmodule.java
@@ -12,18 +12,24 @@ import java.io.IOException;
 public class Crypteringsmodule {
     private Object[] module = new Object[7];
     /* module[  ]
+    ------- globala -------------------------------------
     0 = id
     1 = String or File
     2 = Encrypt or Decrypt
-    3 = IV
-    4 = key
-    -----------------
-    5 = Playne text
-    6 = Encrypted text
-    ------------------
-    5 = in put File
-    6 = ou put File
-    ----------------
+    ----------------------------------------------------------
+    -------- aes -----------------------------------------
+        3 = IV
+        4 = key
+    --------- String ------------------------------------
+            5 = Playne text
+            6 = Encrypted text
+    ---------- File ---------------------------------------
+            5 = in put File
+            6 = ou put File
+    ----------------------------------------------------------
+    -------- ras -----------------------------------------
+    ----------------------------------------------------------
+
      */
     private String[] filestrings = new String[2];
     private Settings settings;
@@ -62,41 +68,74 @@ public class Crypteringsmodule {
 
     public void start() {
         if (module[0].equals((byte) 1)) {
-            if ((boolean) module[1]) {
-                try {
-                    looger.write("string:\n" + module[5] + "\ntiden crypteringen började: \n" + System.nanoTime() +
-                            "\nden krypterade strengen blev:\n");
-                    looger.flush();
-                    String out;
-                    if ((boolean) module[2]) { // 2 = Encrypt or Decrypt
-                        out = Cryptaes.Stringcry((Boolean) module[2], (IvParameterSpec) module[3], (SecretKey) module[4], (String) module[5]);
-                    } else {
-                        out = Cryptaes.Stringcry((Boolean) module[2], (IvParameterSpec) module[3], (SecretKey) module[4], (String) module[5]);
-                    }
-                    looger.write(out);
-                    looger.flush();
-                    looger.close();
-                    System.exit(0);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.exit(-1);
-                }
+            if ((boolean) module[1]) { // 1 = String or File
+                AESs();
             } else {
-                try {
-                    looger.write("File: " + filestrings[0] + "\ntiden crypteringen började: \n" + System.nanoTime() +
-                            "\nden krypterade filen är har:\n" + filestrings[1]);
-                    looger.flush();
-                    looger.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                if ((boolean) module[2]) {
-                    Cryptaes.Filebufercry((byte) 1, (IvParameterSpec) module[3], (SecretKey) module[4], (File) module[5], (File) module[6]);
-                } else {
-                    Cryptaes.Filebufercry((byte) 2, (IvParameterSpec) module[3], (SecretKey) module[4], (File) module[5], (File) module[6]);
-                }
-                System.exit(0);
+                AESf();
             }
-        } //
+        } else if (module[0].equals((byte) 2)) {
+            if ((boolean) module[1]) {
+                RESs();
+            } else {
+                RESf();
+            }
+        }
+    }
+
+    private void AESs() {
+        try {
+            looger.write("string:\n" + module[5] + "\ntiden crypteringen började: \n" + System.nanoTime() +
+                    "\nden krypterade strengen blev:\n");
+            looger.flush();
+            String out;
+            if ((boolean) module[2]) { // 2 = Encrypt or Decrypt
+                out = Cryptaes.Stringcry((Boolean) module[2], (IvParameterSpec) module[3], (SecretKey) module[4], (String) module[5]);
+            } else {
+                out = Cryptaes.Stringcry((Boolean) module[2], (IvParameterSpec) module[3], (SecretKey) module[4], (String) module[5]);
+            }
+            looger.write(out);
+            looger.flush();
+            looger.close();
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+    private void AESf() {
+        try {
+            looger.write("File: " + filestrings[0] + "\ntiden crypteringen började: \n" + System.nanoTime() +
+                    "\nden krypterade filen är har:\n" + filestrings[1]);
+            looger.flush();
+            looger.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        if ((boolean) module[2]) {
+            Cryptaes.Filebufercry((byte) 1, (IvParameterSpec) module[3], (SecretKey) module[4], (File) module[5], (File) module[6]);
+        } else {
+            Cryptaes.Filebufercry((byte) 2, (IvParameterSpec) module[3], (SecretKey) module[4], (File) module[5], (File) module[6]);
+        }
+        System.exit(0);
+    }
+
+    private void RESs() {
+        try {
+            if ((boolean) module[2]) {
+
+            }   else {
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void RESf() {
+        if ((boolean) module[2]) {
+
+        }   else {
+
+        }
     }
 }
