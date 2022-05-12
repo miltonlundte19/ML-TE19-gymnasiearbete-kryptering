ca4bd224dd15628a1c049b09fac57e7ac0446154
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-05-12 13:32:12 +0200

    början på egen variabel för att lagra filen och filens path i string

---
+++ src/setings/Settingsfile.java
@@ -0,0 +1,22 @@
+package setings;
+
+import java.io.File;
+import java.io.Serializable;
+
+public class Settingsfile implements Serializable {
+    private File in;
+    private String fileinstring;
+    private File ou;
+    private String fileoustring;
+
+
+    public void setIN(File in) {
+        this.in = in;
+        fileinstring = in.getAbsolutePath();
+    }
+
+    public void setOU(File ou) {
+        this.ou = ou;
+        fileoustring = ou.getAbsolutePath();
+    }
+}
