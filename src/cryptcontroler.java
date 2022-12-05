a1a1b7b7e4d621dc854fce8e5f5e7eb30f687445
miltonlundte19 <milton.lund.ml@gmail.com>
2022-12-05 16:59:31 +0100

    början på att förklara klasserna

--- src/cryptcontroler.java
+++ src/cryptcontroler.java
@@ -6,12 +6,17 @@ import java.io.*;
 public class cryptcontroler {
     public static void main(String[] args) {
         long logtime = System.nanoTime();
+        // Börjar med att sparar tiden som programet börjar.
+
         File setingsfile = new File("setingsfile.txt");
         if (!setingsfile.exists()) {
             System.out.println("filen fins inte");
             System.exit(-1);
         }
+        // Hitar en fill som har parametrarna för att automatiskt köra programet och titar så att filen existerar.
+
         File startupfile = new File("startuptime.txt");
+        // en fill som loggar skrivs till.
         FileWriter startup;
         try {
             if (!startupfile.exists()) {
@@ -20,16 +25,22 @@ public class cryptcontroler {
             startup = new FileWriter(startupfile);
             startup.write("start time:\n" + logtime + "\n");
             startup.flush();
+            /* skriver tiden som programet startade i logg filen
+               så att men kan räkna ut hur lång tid upstarten tog.
+            */
             ObjectInputStream setingsreder = new ObjectInputStream(new FileInputStream(setingsfile));
             Settings settings = (Settings) setingsreder.readObject();
+            // Läser in en egen jord variabel som har alltför krypteringen för dig gjort
+            // mer om Settings varabeln fins /setings/Settings
+
             Crypteringsmodule crypteringsmodule = new Crypteringsmodule(settings);
             crypteringsmodule.setLooger(startup);
             crypteringsmodule.start();
+            // ladar in några objekt till krypteringen och sedan startar klasen.
+
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
         }
-
-
     }
 }
--- src/modules/Crypteringsmodule.java
+++ src/modules/Crypteringsmodule.java
@@ -16,7 +16,9 @@ import java.security.spec.RSAPrivateKeySpec;
 
 public class Crypteringsmodule {
     private Object[] module = new Object[7];
-    /* module[  ]
+    /* Visar vilka värden som hamnav vart beroende på vilken krypterings metod som används
+     och om det är en text eller fill.
+     module[  ]
     ------- globala -------------------------------------
     0 = id
     1 = String or File
@@ -46,16 +48,21 @@ public class Crypteringsmodule {
     private Settings settings;
     private FileWriter looger;
 
+    // startar kass för att få in värderna på rätt stäle.
     public Crypteringsmodule(Settings settings) {
         module[0] = settings.getId();
         module[1] = settings.getStringORfile();
         module[2] = settings.getEncryptORdecrypt();
         manulesnapshotAlurt = settings.getManulesnapshot();
+        // hämtar datan från settings till en objekt array så att det ska vara lättare att läga in datan.
+
         if (module[0].equals((byte) 1)) {
             AESset(settings.getAes());
         } else if (module[0].equals((byte) 2)) {
             RESset(settings.getRes());
-        }
+        } // titar efter vilken krypterings model som ska användas
+
+        // anvends inte men är fortfarande kvar (komer att ta bort).
         this.settings = settings;
     }
 
@@ -64,7 +71,7 @@ public class Crypteringsmodule {
         module[4] = aes.getKey();
         if ((boolean) module[1]) {
             module[5] = aes.getPlainText();
-        } else {
+        } else { // titar om det är en string eller fil
             Settingsfile fileseter = aes.getFileInOu();
             setFilemod(fileseter);
         }
@@ -73,26 +80,33 @@ public class Crypteringsmodule {
     private void setFilemod(Settingsfile fileseter) {
         filestrings[0] = fileseter.getFileinstring();
         filestrings[1] = fileseter.getFileoustring();
-        File file = fileseter.getIn();
+        // Hämtar filernas plats i string format
+        File file = fileseter.getIn();// titar om den kan hämta filen från ett fill objekt
         if (!file.exists()) {
             file = new File(filestrings[0]);
-        }
+        } // om inte så görden en ny fil och försöker med filens absoluta väg ((men det kanske inte gör någonting))
         module[5] = file;
+        // setter in filen och gör samma sak med filen som den ska skriva till
         file = fileseter.getOu();
         if (!file.exists()) {
             file = new File(filestrings[1]);
         }
         module[6] = file;
+        // krypteringen behöver en ut fill eftersom att den skriver inte över orginala fillen utan krypterar/dekrypterar
+        // inehålet av fillen (som en lång string)
     }
     private void RESset(RESsettings res) {
         File keyfile = res.getKeyfile();
         if (!keyfile.exists()) {
             keyfile = new File(res.getKeyfilepath());
-        }
+        }// hämtar filen som kykeln till RES krypteringen
+
         getpubOrpri(keyfile, res.isPriORpub());
+        // en funktion som hämtar och byger i-hop nykeln
+
         if ((boolean) module[1]) {
             module[5] = res.getMesige();
-        } else {
+        } else { // titar om det är en string eller fil
             Settingsfile fileseter = res.getFileInOu();
             setFilemod(fileseter);
         }
@@ -101,6 +115,7 @@ public class Crypteringsmodule {
     private void getpubOrpri(File keyfile, boolean priOrpub) {
         try {
             ObjectInputStream inputStream = new ObjectInputStream(new BufferedInputStream(new FileInputStream(keyfile)));
+            // använder en ObjectInputStream eftersom att BigInteger inte är Serializable
             BigInteger modulus = (BigInteger) inputStream.readObject();
             BigInteger exponent = (BigInteger) inputStream.readObject();
             inputStream.close();
@@ -120,9 +135,9 @@ public class Crypteringsmodule {
 
     public void setLooger(FileWriter looger) {
         this.looger = looger;
-    }
+    } // för över loog fillens skrivare.
 
-    public void start() {
+    public void start() { // Starten på krypteringen. Funktionerna med en s är för String och f är för File.
         if (module[0].equals((byte) 1)) {
             if ((boolean) module[1]) { // 1 = String or File
                 AESs();
@@ -138,14 +153,17 @@ public class Crypteringsmodule {
         }
     }
 
+    // upstarten för AES krypteringen med några skilnader melan dem som mest hanlar om skriva ner resultatet.
     private void AESs() {
         try {
             looger.write("string:\n" + module[5] + "\ntiden aes crypteringen började: \n" + System.nanoTime() +
                     "\nden krypterade strengen blev:\n");
+            // skriver ner vad stringen var och tiden det tog att sätta upp allt
             looger.flush();
             String out;
 
             out = Cryptaes.Stringcry((Boolean) module[2], (IvParameterSpec) module[3], (SecretKey) module[4], (String) module[5]);
+            // Här krypteraren och får utt den krypterade strengen som sedan blir nedsriven.
 
             looger.write(out + '\n');
             if (manulesnapshotAlurt) {
@@ -155,6 +173,8 @@ public class Crypteringsmodule {
             looger.close();
             if (manulesnapshotAlurt) {
                 JOptionPane.showMessageDialog(null, "keypteringen slutate, ta snap");
+                // Är här för att seja till att programet är färdigt och att ta en sapshot i programet som används
+                // för att mäta programet (visa gör det inte automatiskt)
             }
         } catch (IOException e) {
             e.printStackTrace();
