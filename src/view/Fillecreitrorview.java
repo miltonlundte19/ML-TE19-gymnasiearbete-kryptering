851ea872bbc044d73b4a209f701dac647460c4d3
miltonlundte19 <milton.lund.ml@gmail.com>
2023-05-10 15:05:33 +0200

    lagd till dem grafiska kompånenterna till alla views

--- src/view/Fillecreitrorview.java
+++ src/view/Fillecreitrorview.java
@@ -1,4 +1,22 @@
 package view;
 
+import javax.swing.*;
+
 public class Fillecreitrorview {
+    private JPanel panel1;
+    private JLabel ivlabel;
+    private JComboBox comboBox1;
+    private JPanel ivselekterpanel;
+    private JPanel pasvirdpanel;
+    private JLabel Passwirde;
+    private JButton selektpasbutton;
+    private JButton geniraitepasbutton;
+    private JTextField pasvirddisplayfield;
+    private JPanel filesilektorpanel;
+    private JLabel filemesigelabel;
+    private JButton button1;
+    private JTextField textField1;
+    private JCheckBox checkBox1;
+    private JButton button2;
+    private JTextField textField2;
 }
--- src/view/Settingsview.java
+++ src/view/Settingsview.java
@@ -1,4 +1,21 @@
 package view;
 
+import javax.swing.*;
+
 public class Settingsview {
+    private JPanel panel1;
+    private JPanel filegenirator;
+    private JPanel siseinput;
+    private JPanel creationpanel;
+    private JTextField DirektorytextField;
+    private JTextField FileNamntextField;
+    private JButton DirectoryButton;
+    private JButton TestFilebutton;
+    private JLabel DirectoryStatesLabel;
+    private JLabel FileNameTestStateslabel;
+    private JLabel GBlabel;
+    private JTextField GBtextField;
+    private JButton Okbutton;
+    private JLabel StatusLabel;
+    private JLabel StatusValueLabel;
 }
