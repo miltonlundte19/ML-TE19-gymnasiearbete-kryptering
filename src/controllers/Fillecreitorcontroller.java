a5c399b13e4a500a0b19942136fbaf59ec521ea1
miltonlundte19 <milton.lund.ml@gmail.com>
2023-05-12 12:48:41 +0200

    start on the controller for the file creator

--- src/controllers/Fillecreitorcontroller.java
+++ src/controllers/Fillecreitorcontroller.java
@@ -1,4 +1,30 @@
 package controllers;
 
+import modules.Fillecriatormodel;
+import view.Fillecreitrorview;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Fillecreitorcontroller {
+    private Fillecreitrorview view;
+    private Fillecriatormodel modell;
+
+    public static void main(String[] args) {
+        Fillecreitorcontroller fillecreitorcontroller = new Fillecreitorcontroller();
+    }
+    public Fillecreitorcontroller() {
+        view = new Fillecreitrorview();
+        modell = new Fillecriatormodel();
+
+        JFrame frame = new JFrame("Fillecreitrorview");
+        frame.setContentPane(view.getPanel1());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
