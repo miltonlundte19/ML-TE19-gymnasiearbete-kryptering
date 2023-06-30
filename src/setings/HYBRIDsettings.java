00ea981748821c7b0de41c71a65c89675f10f57c
miltonlundte19 <milton.lund.ml@gmail.com>
2023-06-30 12:17:17 +0200

    get funktions for HYBRIDsettings

--- src/setings/HYBRIDsettings.java
+++ src/setings/HYBRIDsettings.java
@@ -1,10 +1,41 @@
 package setings;
 
 public class HYBRIDsettings {
-    private Boolean keyinfile;
+    private boolean keyinfile;
     private boolean storkeyInFile;
 
-    private Boolean Aes;
-    private Boolean Rsa;
+    private boolean Aes;
+    private boolean Rsa;
 
+    public boolean isKeyinfile() {
+        return keyinfile;
+    }
+
+    public void setKeyinfile(boolean keyinfile) {
+        this.keyinfile = keyinfile;
+    }
+
+    public boolean isStorkeyInFile() {
+        return storkeyInFile;
+    }
+
+    public void setStorkeyInFile(boolean storkeyInFile) {
+        this.storkeyInFile = storkeyInFile;
+    }
+
+    public boolean isAes() {
+        return Aes;
+    }
+
+    public void setAes(boolean aes) {
+        Aes = aes;
+    }
+
+    public boolean isRsa() {
+        return Rsa;
+    }
+
+    public void setRsa(boolean rsa) {
+        Rsa = rsa;
+    }
 }
